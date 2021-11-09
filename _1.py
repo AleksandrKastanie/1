a87fe49c9e960979ba73d6d83850e7ec63fe8355
opilane <opilane@HP-CZC6297B8C>
2021-11-09 16:37:53 +0200

    Add project files.

---
+++ _1.py
@@ -0,0 +1,48 @@
+#from random import *
+#p=0
+#n=0
+#N=randint(1,10)
+#print(N)
+#for i in range(N):
+#    arv=int(input("Sisesta arv"))
+#    if arv>0:
+#        p+=1
+#    elif arv<0:
+#        n+=1
+#print("Neg: "+str(n))
+#print("Pos: "+str(p))
+#print("Nullid: "+str(N-n-p))
+
+
+#7-------------------------
+#from random import *
+##A<B
+#A=randint(10,100)
+#B=randint(100,1000)
+#print("A="+str(A))
+#print("B="+str(B))
+#K=int(input("K: "))
+#for i in range(A,B+1):
+#    if i%K==0: print(i)
+#4----------------------
+#for i in range(10,21):
+   # print(i**2)
+
+#1----------------------------
+#p=0;
+#for i in range(15):#i=0,1,2,3...
+#    A=0
+#    while type(A)!=float:
+#        try:
+#            A=float(input("Sisesta "+str(i+1)+" arv"))
+#        except TypeError:
+#            print("Mmmmmmm!")
+#    if A==round(A):
+#        p+=1
+#        print(str(A)+"on täisarv")
+#    else:
+#        print(str(A)+" ei ole täisarv")
+
+#print (str(p)+" Täisarvud")
+
+
